eb2812f1171631e34611b39b9c86a7654271e9b0
caroline.fernandes <carolinefernandesc@hotmail.com>
2024-11-26 16:04:46 -0300

    Iniciando responsividade skills

--- src/pages/About.js
+++ src/pages/About.js
@@ -14,11 +14,11 @@ function About(){
                 Moro em São Gonçalo do Amarante/ RN.<br />
                 Hoje me encontro apaixonada pela área de progamação,<br />
                 atualmente faço Graduação em Sistemas de Informação,<br />
-                com formação em Desenvolvimento Web, pela Trybe<br />
+                com formação em Desenvolvimento Web, pela Trybe,<br />
                 e sou Técnica em Comércio pelo Instituto Federal do RN.<br />
                 Sou uma pessoa apaixonada pelos desafios da vida.<br />
                 Vivo em busca das minhas realizações pessoais/ profissionais.<br />
-                Aqui ao lado são algumas fotos minhas aleatórias, rsrs.<br />
+                Aqui tem algumas fotos minhas aleatórias, rsrs.<br />
                 Desde já agradeço sua visita.<br />
                 Fique à vontade para entrar em contato comigo!
                 </h1>
